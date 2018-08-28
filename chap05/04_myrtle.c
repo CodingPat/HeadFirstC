0a2de985fad0107eeb8caf516785dcd643150080
woody <woody@tallahassee.local>
2018-08-28 16:10:35 +0200

    chapter05 in progress

--- chap05/04_myrtle.c
+++ chap05/04_myrtle.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(){
+typedef struct{
+	const char* name;
+	const char* species;
+	int age;
+}turtle;
 
+
+void happy_birthday(turtle *t){
+	t->age=t->age+1;
+	printf("Happy birthday %s, you're now %i years old !\n",t->name,t->age);	
+}
+
+
+int main(){
+	turtle myrtle={"Myrtle","Leatherback sea turtle",99};
+	happy_birthday(&myrtle);
+	printf("Your age is now : %i\n",myrtle.age);
 }
 
